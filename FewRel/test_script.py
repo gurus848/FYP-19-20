35cff9c6370258443dcb41a789ff3106aa08be61
Guru Sarjith Senthil <gurus848@gmail.com>
2020-01-19 23:41:23 +0530

    implemented using ner to automatically find the entities that need the relation to be determined.

--- FewRel/test_script.py
+++ FewRel/test_script.py
@@ -6,6 +6,7 @@ import os
 import torch
 import spacy
 import neuralcoref
+from itertools import combinations
 
 #RUNS USING GPU if available
 
@@ -57,97 +58,103 @@ na_rate = 0
 example_relation_data = [
     {'name':'love',
     'examples':[
-        {'sentence':'meow loves mo', 'head':'meow', 'tail':'mo'},
-        {'sentence':'tom is in love with jull', 'head':'tom', 'tail':'jull'}
+        {'sentence':'Meow loves Mo', 'head':'Meow', 'tail':'Mo'},
+        {'sentence':'Tom is in love with Jull', 'head':'Tom', 'tail':'Jull'}
     ]},
     {'name':'hate',
     'examples':[
-        {'sentence':'trump hates the mooch', 'head':'trump', 'tail':'mooch'},
-        {'sentence':'ivanka and jared dislike each other intensely', 'head':'ivanka', 'tail':'jared'}
+        {'sentence':'Trump hates the Mooch', 'head':'Trump', 'tail':'Mooch'},
+        {'sentence':'Ivanka and Jared dislike each other intensely', 'head':'Ivanka', 'tail':'Jared'}
     ]},
     {'name':'spouse',
     'examples':[
-        {'sentence':'trump is married to ivanka', 'head':'trump', 'tail':'ivanka'},
-        {'sentence':"bill went out with his wife jill on saturday", 'head':'bill', 'tail':'jill'}
+        {'sentence':'Trump is married to Ivanka', 'head':'Trump', 'tail':'Ivanka'},
+        {'sentence':"Bill went out with his wife Jill on saturday", 'head':'Bill', 'tail':'Jill'}
     ]},
         {'name':'insult',
     'examples':[
-        {'sentence':'The president said that michael cohen is a rat', 'head':'president', 'tail':'michael'},
-        {'sentence':'meow and tom threw jabs at each other', 'head':'meow', 'tail':'tom'}
+        {'sentence':'The President said that Michael Cohen is a rat', 'head':'The President', 'tail':'Michael'},
+        {'sentence':'Meow and Tom threw jabs at each other', 'head':'Meow', 'tail':'Tom'}
     ]},
         {'name':'capital',
     'examples':[
-        {'sentence':'austin is the capital of texas', 'head':'austin', 'tail':'texas'},
-        {'sentence':"the capital of china is located in beijing", 'head':'beijing', 'tail':"china"}
+        {'sentence':'Austin is the capital of Texas', 'head':'Austin', 'tail':'Texas'},
+        {'sentence':"the capital of China is located in Beijing", 'head':'Beijing', 'tail':"China"}
     ]}
     
 ]
 
 queries = [{
-    'sentence':'furball and fluffy are very loving to each other','head':'furball','tail':'fluffy'
+    'sentence':'Cohen and Fluffy are very loving to each other','head':'Cohen','tail':'Fluffy'
 },
 {
-    'sentence':'washington is the capital of the US','head':'washington','tail':'US'
+    'sentence':"""The US's capital is Washington""",'head':'Washington','tail':'US'
 }]
 
 nlp = spacy.load("en_core_web_sm")
 neuralcoref.add_to_pipe(nlp)
 
-max_length = 128
+def spacy_tokenize(sentence):
+    doc = nlp(sentence)
+    return list(map(str, nlp(doc._.coref_resolved)))
+
+def get_head_tail_pairs(sentence):
+    acceptable_entity_types = ['PERSON', 'NORP', 'ORG', 'GPE', 'PRODUCT', 'EVENT', 'LAW', 'LOC', 'FAC']
+    doc = nlp(sentence)
+    doc = nlp(doc._.coref_resolved)
+    entity_info = [(X.text, X.label_) for X in doc.ents]
+    entity_info = set(map(lambda x:x[0], filter(lambda x:x[1] in acceptable_entity_types, entity_info)))
+
+    return combinations(entity_info, 2)
+    
+
+max_length = 128   #max length in terms of the number of characters
 for q in queries:
     fusion_set = {'word': [], 'mask': [], 'seg': []}
-    doc = nlp(q['sentence'])
-    doc = nlp(doc._.coref_resolved)  #use neuralcoref to do coreference resolution and then do NER again
-    tokens = list(map(str, doc))
-    
-    #TODO: use NER entities to find all pairs to check
-    
-    tokenized_head = list(map(str, nlp(q['head'])))  #TODO: remove once the NER pair thing has been written.
-    tokenized_tail = list(map(str, nlp(q['tail'])))
-    
-    head_indices = list(range(tokens.index(tokenized_head[0]), tokens.index(tokenized_head[0])+len(tokenized_head)))   
-    tail_indices = list(range(tokens.index(tokenized_tail[0]), tokens.index(tokenized_tail[0])+len(tokenized_tail)))
-    bert_query_tokens = bert_tokenize(tokens, head_indices, tail_indices)
-    for relation in example_relation_data:
-        for ex in relation['examples']:
-            
-            doc = nlp(ex['sentence'])
-            doc = nlp(doc._.coref_resolved)  #use neuralcoref to do coreference resolution, and then do tokenization. coreference resolution is not a perfect process
-            tokens = list(map(str, doc))
-            
-            tokenized_head = list(map(str, nlp(ex['head'])))   #head and tail spelling and punctuation should match the corefered output exactly
-            tokenized_tail = list(map(str, nlp(ex['tail'])))
-            head_indices = list(range(tokens.index(tokenized_head[0]), tokens.index(tokenized_head[0])+len(tokenized_head)))   
-            tail_indices = list(range(tokens.index(tokenized_tail[0]), tokens.index(tokenized_tail[0])+len(tokenized_tail)))
-            bert_relation_example_tokens = bert_tokenize(tokens, head_indices, tail_indices)
-            
-            SEP = sentence_encoder.tokenizer.convert_tokens_to_ids(['[SEP]'])
-            CLS = sentence_encoder.tokenizer.convert_tokens_to_ids(['[CLS]'])
-            word_tensor = torch.zeros((max_length)).long()
-            
-            new_word = CLS + bert_relation_example_tokens + SEP + bert_query_tokens + SEP
-            for i in range(min(max_length, len(new_word))):
-                word_tensor[i] = new_word[i]
-            mask_tensor = torch.zeros((max_length)).long()
-            mask_tensor[:min(max_length, len(new_word))] = 1
-            seg_tensor = torch.ones((max_length)).long()
-            seg_tensor[:min(max_length, len(bert_relation_example_tokens) + 1)] = 0
-            fusion_set['word'].append(word_tensor)
-            fusion_set['mask'].append(mask_tensor)
-            fusion_set['seg'].append(seg_tensor)
+    tokens = spacy_tokenize(q['sentence'])
     
     
-    fusion_set['word'] = torch.stack(fusion_set['word'])
-    fusion_set['seg'] = torch.stack(fusion_set['seg'])
-    fusion_set['mask'] = torch.stack(fusion_set['mask'])
-    
-    if torch.cuda.is_available():
-        fusion_set['word'] = fusion_set['word'].cuda()
-        fusion_set['seg'] = fusion_set['seg'].cuda()
-        fusion_set['mask'] = fusion_set['mask'].cuda()
+    for head, tail in get_head_tail_pairs(q['sentence']):  #iterating through all possible combinations of 2 named entities
+        tokenized_head = spacy_tokenize(head)
+        tokenized_tail = spacy_tokenize(tail)
+        head_indices = list(range(tokens.index(tokenized_head[0]), tokens.index(tokenized_head[0])+len(tokenized_head)))   
+        tail_indices = list(range(tokens.index(tokenized_tail[0]), tokens.index(tokenized_tail[0])+len(tokenized_tail)))
+        bert_query_tokens = bert_tokenize(tokens, head_indices, tail_indices)
+        for relation in example_relation_data:
+            for ex in relation['examples']:
+                tokens = spacy_tokenize(ex['sentence'])
+                tokenized_head = spacy_tokenize(ex['head'])  #head and tail spelling and punctuation should match the corefered output exactly
+                tokenized_tail = spacy_tokenize(ex['tail'])
+                head_indices = list(range(tokens.index(tokenized_head[0]), tokens.index(tokenized_head[0])+len(tokenized_head)))
+                tail_indices = list(range(tokens.index(tokenized_tail[0]), tokens.index(tokenized_tail[0])+len(tokenized_tail)))
+                bert_relation_example_tokens = bert_tokenize(tokens, head_indices, tail_indices)
+
+                SEP = sentence_encoder.tokenizer.convert_tokens_to_ids(['[SEP]'])
+                CLS = sentence_encoder.tokenizer.convert_tokens_to_ids(['[CLS]'])
+                word_tensor = torch.zeros((max_length)).long()
+
+                new_word = CLS + bert_relation_example_tokens + SEP + bert_query_tokens + SEP
+                for i in range(min(max_length, len(new_word))):
+                    word_tensor[i] = new_word[i]
+                mask_tensor = torch.zeros((max_length)).long()
+                mask_tensor[:min(max_length, len(new_word))] = 1
+                seg_tensor = torch.ones((max_length)).long()
+                seg_tensor[:min(max_length, len(bert_relation_example_tokens) + 1)] = 0
+                fusion_set['word'].append(word_tensor)
+                fusion_set['mask'].append(mask_tensor)
+                fusion_set['seg'].append(seg_tensor)
+
+        fusion_set['word'] = torch.stack(fusion_set['word'])
+        fusion_set['seg'] = torch.stack(fusion_set['seg'])
+        fusion_set['mask'] = torch.stack(fusion_set['mask'])
         
-    logits, pred = model(fusion_set, N, K, 1)
-    print('Sentence: \"{}\", prediction: {}'.format(q['sentence'], example_relation_data[pred.item()]['name']))   #todo: handle na case, which would be out of bounds
+        if torch.cuda.is_available():
+            fusion_set['word'] = fusion_set['word'].cuda()
+            fusion_set['seg'] = fusion_set['seg'].cuda()
+            fusion_set['mask'] = fusion_set['mask'].cuda()
+        
+        logits, pred = model(fusion_set, N, K, 1)
+        print('Sentence: \"{}\", head: \"{}\", tail: \"{}\", prediction: {}'.format(q['sentence'], head, tail, example_relation_data[pred.item()]['name']))   #TODO: handle na case, which would be out of bounds
     
             
             
